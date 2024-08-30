ede684d1a5a0a103c429f3d51afb3069f417b5c8
maiko254 <bonyomichael406.com>
2024-08-30 11:38:37 +0300

    Express server with API that returns status of Redis and MongoDB and number of documents in MongoDB collections users and files

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(req, res) {
+    const dbStatus = dbClient.isAlive();
+    const redisStatus = redisClient.isAlive();
+
+    res.status(200).send({ redis: redisStatus, db: dbStatus });
+  }
+
+  static async getStats(req, res) {
+    const userCount = await dbClient.nbUsers();
+    const fileCount = await dbClient.nbFiles();
+
+    res.status(200).send({ users: userCount, files: fileCount });
+  }
+}
+
+export default AppController;
